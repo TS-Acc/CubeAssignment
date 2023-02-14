61a91e555ab2bd11238493972f1e26ea1a22827f
Trevor Shepherd <t.rayshpd@gmail.com>
2023-02-14 10:24:05 -0500

    Add project files.

---
+++ CubeAssignment/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// Using a while loop to print the cube of the odd numbers from 1 to 25
+
+
+var oddNumbers = 1;
+
+while(oddNumbers <= 25)
+{
+    Console.WriteLine($"Odd number: {oddNumbers}, odd number cubed: {oddNumbers * oddNumbers * oddNumbers}");
+    oddNumbers += 2;
+}
+
+
+
